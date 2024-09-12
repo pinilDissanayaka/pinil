53ae68db5cb8a046658159614e427ddf60f7c403
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-09-12 11:26:08 +0530

    add

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,13 +3,10 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home_social">
-        <a href="https://www.facebook.com/profile.php?id=100008628512280" className="home_social-icon" target="_blank">
-            <i className="uil uil-facebook"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/shehan-lakshitha-8ab6a9218/" className="home_social-icon" target="_blank">
+        <a href="https://www.linkedin.com/in/pinil-dissanayaka-a69a41285/" className="home_social-icon" target="_blank">
             <i className="uil uil-linkedin-alt"></i>
         </a>
-        <a href="https://github.com/Shehan-lakshitha" className="home_social-icon" target="_blank">
+        <a href="https://github.com/pinilDissanayaka" className="home_social-icon" target="_blank">
             <i className="uil uil-github-alt"></i>
         </a>
     </div>
