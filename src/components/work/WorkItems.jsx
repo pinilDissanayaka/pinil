d1555895d6bb8e4c2a2db1b1e8fc1ea427aad796
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-09-23 08:14:43 +0530

    change buttom target

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -6,12 +6,12 @@ const WorkItems = ({ item }) => {
       <img src={item.image} alt="" className="work_img" />
       <h3 className="work_title">{item.title}</h3>
       <div className="links">
-        <a href={item.github} className="work_button">
+        <a href={item.github} className="work_button" target="_blank">
           View on GitHub<i className="bx bxl-github work_button-icon"></i>{" "}
         </a>
 
         {item.demo !== "" ? (
-          <a href={item.demo} className="work_button">
+          <a href={item.demo} className="work_button" target="_blank">
             Demo<i className="bx bx-right-arrow-alt work_button-icon"></i>{" "}
           </a>
         ) : (
