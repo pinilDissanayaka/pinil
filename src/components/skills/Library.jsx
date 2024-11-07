5645eac01e8390490b72df8c40cd7179e771b379
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-11-07 15:14:02 +0530

    add skill

--- src/components/skills/Library.jsx
+++ src/components/skills/Library.jsx
@@ -70,6 +70,14 @@ const Library = () => {
                 <h3 className="skills_name">OpenCV</h3>
               </div>
             </div>
+             
+            <div className="skills_data">
+              <i className="bx bx-badge-check skills_icon"></i>
+
+              <div>
+                <h3 className="skills_name">PySpark</h3>
+              </div>
+            </div>
           </div>
 
           <div className="skills_group">
